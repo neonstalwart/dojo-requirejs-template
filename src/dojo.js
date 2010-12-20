f34fd082199e665e52efb20dbdab8eb8fe17f356
ben hockey <neonstalwart@gmail.com>
2010-12-20 10:31:32 -0500

    update to use requirejs 0.2.1

--- src/dojo.js
+++ src/dojo.js
@@ -3,13 +3,13 @@ require([
 	'order!dojo/_base/_loader/loader.js',
 	'order!dojo/_base/_loader/hostenv_browser.js',
 	// this list of includes is needed because of the conditional used in dojo/_base.js
-	'order!dojo/_base/lang.js',
-	'order!dojo/_base/array.js',
-	'order!dojo/_base/declare.js',
-	'order!dojo/_base/connect.js',
-	'order!dojo/_base/Deferred.js',
-	'order!dojo/_base/json.js',
-	'order!dojo/_base/Color.js',
-	'order!dojo/_base/browser.js'
+	'order!dojo/_base/lang',
+	'order!dojo/_base/array',
+	'order!dojo/_base/declare',
+	'order!dojo/_base/connect',
+	'order!dojo/_base/Deferred',
+	'order!dojo/_base/json',
+	'order!dojo/_base/Color',
+	'order!dojo/_base/browser'
 ], function () {});
 
