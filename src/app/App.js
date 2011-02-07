e977ed213a6d7e48f0482c22a2b03e997bb6e21d
ben hockey <neonstalwart@gmail.com>
2011-02-07 10:51:12 -0500

    added some extra locale dependent code

--- src/app/App.js
+++ src/app/App.js
@@ -3,14 +3,23 @@ define([
 	'dijit/_Widget',
 	'dijit/_Templated',
 	'i18n!./nls/App',
-	'text!./templates/App.html'
-], function (d, Widget, Templated, i18n, template) {
+	'text!./templates/App.html',
+	'dojo/date/locale',
+	'i18n!dojo/cldr/nls/fr-ch/gregorian'
+], function (d, Widget, Templated, i18n, template, locale) {
 	return d.declare([
 		Widget,
 		Templated
 	], {
 		templateString: template,
 		i18n: i18n,
+
+		buildRendering: function () {
+			this.defaultMonths = locale.getNames('months', 'wide', 'standAlone', d.locale).join(', ');
+			this.frchMonths = locale.getNames('months', 'wide', 'standAlone', 'fr-ch').join(', ');
+			this.inherited(arguments);
+		},
+
 		startup: function () {
 			this.inherited(arguments);
 			if (this.alertWhenStarted) {
